d25e1a273536798df418718ef5dbce0b49802a5a
aditya764k <139098070+aditya764k@users.noreply.github.com>
2025-06-17 23:21:42 +0530

    Add files via upload
    Task 1: Perform Basic Mathematical Operations

---
+++ M1_Task1.py
@@ -0,0 +1,7 @@
+a = int(input("enter the first number : "))
+b = int(input("enter the second number: "))
+
+print("Addition: ", a+b)
+print("subtraction: ", a-b)
+print("Multiplication: ",a*b)
+print("Divsion: ", a/b)
