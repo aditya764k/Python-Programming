74bd2260936c86e39d70d6bedf315f9656b49536
aditya764k <139098070+aditya764k@users.noreply.github.com>
2025-06-17 23:22:27 +0530

    Add files via upload
    Task 2: Create a Personalized Greeting

---
+++ M1_Task2.py
@@ -0,0 +1,6 @@
+from re import fullmatch
+
+First_name = input("enter the First name : ")
+Last_name = input("enter the last name : ")
+Fullname = First_name+" "+Last_name+"!"
+print("Hello, ",Fullname," Welcome to the Python program")
